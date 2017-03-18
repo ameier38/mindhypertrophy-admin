a932e7cbd7bc37cd0d04736144173b36a3af3c1e
Andrew Meier <ameier38@gmail.com>
2017-03-18 09:39:07 -0400

    update config and app title

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import { CardList, CardEdit, CardCreate } from './cards';
 import { TagList, TagEdit, TagCreate } from './tags';
 import restClient from './restClient'
 
+const adminConfig = {
+    title: "Mind Hypertrophy Admin",
+    restClient,
+    authClient
+}
+
 const App = () => (
-    <Admin restClient={restClient} authClient={authClient}>
+    <Admin {...adminConfig} >
         <Resource name="cards" list={CardList} edit={CardEdit} create={CardCreate} />
         <Resource name="tags" list={TagList} edit={TagEdit} create={TagCreate} />
     </Admin>
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_HOST = process.env.API_HOST || 'localhost'
-const API_PORT = process.env.API_PORT || 5000
+const API_HOST = process.env.REACT_APP_API_HOST || 'localhost'
+const API_PORT = process.env.REACT_APP_API_PORT || 5000
 export const API_URL = `http://${API_HOST}:${API_PORT}/api`
