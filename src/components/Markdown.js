3c24bdc6643226223ad9c826600c19bc609fe563
Andrew Meier <ameier38@gmail.com>
2017-09-08 10:57:18 -0400

    updates

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -28,6 +28,7 @@ class Markdown extends Component{
   renderMarkdown(source) {
     if (!this.markdown) {
       this.markdown = new MarkdownIt({
+        html: true,
         highlight: (str, lang) => {
           if (lang && hljs.getLanguage(lang)) {
             try {
